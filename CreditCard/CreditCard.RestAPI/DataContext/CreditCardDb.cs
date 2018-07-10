329a1e38c36f9d3d62407ee13788019b9c6c3278
DESKTOP-0H2MGJ0\Timmy Limp <timmy.limp@gmail.com>
2018-07-10 21:52:59 +0700

    Enable migration, Initial create database, generate seed

---
+++ CreditCard/CreditCard.RestAPI/DataContext/CreditCardDb.cs
@@ -0,0 +1,13 @@
+﻿using CreditCard.Entities;
+using System.Data.Entity;
+
+namespace CreditCard.RestAPI.DataContext
+{
+    public class CreditCardDb : DbContext
+    {
+        public CreditCardDb() : base("LocalServer") { }
+
+        public DbSet<Card> Cards { get; set; }
+
+    }
+}
---
+++ CreditCard/CreditCard.RestAPI/DataContext/CreditCardMigrations/Configuration.cs
@@ -0,0 +1,30 @@
+namespace CreditCard.RestAPI.DataContext.CreditCardMigrations
+{
+    using CreditCard.Entities;
+    using System.Data.Entity.Migrations;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CreditCard.RestAPI.DataContext.CreditCardDb>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            MigrationsDirectory = @"DataContext\CreditCardMigrations";
+        }
+
+        protected override void Seed(CreditCard.RestAPI.DataContext.CreditCardDb context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+            var cards = new Card[] {
+                new Card { Id = 1, CardNumber = "4111111111111111" }, // Visa
+                new Card { Id = 2, CardNumber = "5111111111111111" }, // MasterCard
+                new Card { Id = 3, CardNumber = "311111111111111" },  // Amex
+                new Card { Id = 4, CardNumber = "3222222222222222" }, // JCB
+                new Card { Id = 5, CardNumber = "6111111111111111" }  // Unknown
+            };
+            context.Cards.AddOrUpdate(cards);
+        }
+    }
+}
