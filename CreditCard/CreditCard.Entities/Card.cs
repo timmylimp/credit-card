1f81e0c37306b9120524c14eeda9abac0cda5352
DESKTOP-0H2MGJ0\Timmy Limp <timmy.limp@gmail.com>
2018-07-10 21:00:49 +0700

    Create card entity

---
+++ CreditCard/CreditCard.Entities/Card.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CreditCard.Entities
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(16)]
+        [MinLength(15)]
+        public string CardNumber { get; set; }
+
+    }
+}
